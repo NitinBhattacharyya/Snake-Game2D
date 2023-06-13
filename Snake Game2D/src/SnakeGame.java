6fd7defb6b094fb4a78dc27b03b0343b7ad53ac5
NitinBhattacharyya <42495409+NitinBhattacharyya@users.noreply.github.com>
2023-06-13 20:22:18 +0530

    First commit

---
+++ Snake Game2D/src/SnakeGame.java
@@ -0,0 +1,20 @@
+import javax.swing.*;
+
+public class SnakeGame extends JFrame {
+    Board board;
+    SnakeGame()
+    {
+        board=new Board();
+        add(board);
+        //sizes the frame so that all its contents are at or above preferred size
+        //resizes the frame to component size that is to the boards dimension
+        pack();
+        setResizable(false);
+        setTitle("Snake Game 2D");
+        setDefaultCloseOperation(SnakeGame.EXIT_ON_CLOSE);
+        setVisible(true);
+    }
+    public static void main(String[] args) {
+        SnakeGame snakeGame=new SnakeGame();
+    }
+}
